4102b6fa36d8095631f880ecbe6cac2d665234e5
Raja Ahirwar <48143548+rajaahirwarofficial@users.noreply.github.com>
2020-10-31 19:57:29 +0530

    Add files via upload
    Student Registration Detabase

---
+++ Success.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Success</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+   <div class="succ">
+    <center><h2>Success Fully Submit</h2></center> 
+   </div>
+   <center><a href="index.html"><input type="button" value="Back" class="success_btn"></a></center>
+</div>
+</body>
+</html>
